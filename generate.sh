7bc024b4ea1bc831745af85f416b3980914c9d19
muffin1998 <chuiqinfan@hotmail.com>
2024-10-13 16:06:09 +0800

    fix ipv6 dns server & tailscale route

--- generate.sh
+++ generate.sh
@@ -21,6 +21,7 @@ echo "  - DIRECT_IPSET_RULES = $DIRECT_IPSET_RULES"
 echo "  - IPTABLES_RULES = $IPTABLES_RULES"
 echo "  - IP_ROUTE_RULES = $IP_ROUTE_RULES"
 echo "  - DIRECT_IP_LIST = $DIRECT_IP_LIST"
+echo "  - RESERVED_IP_LIST = $RESERVED_IP_LIST"
 echo "  - DIRECT_DOMAINS = $DIRECT_DOMAINS"
 echo "  - FORCE_DIRECT_DOMAINS = $FORCE_DIRECT_DOMAINS"
 echo "  - DIRECT_DNS_SERVERS = $DIRECT_DNS_SERVERS"
@@ -32,9 +33,11 @@ echo "  - DNS_PROXY_SERVER_RULES = $DNS_PROXY_SERVER_RULES"
 echo "  - DNS_CONF = $DNS_CONF"
 echo "  - STARTUP_SCRIPT = $STARTUP_SCRIPT"
 echo "  - FORWARD_INTERFACE = $FORWARD_INTERFACE"
+echo "  - ENABLE_IPV6_DNS_SERVER = $ENABLE_IPV6_DNS_SERVER"
 
 # file path
 DIRECT_IP_LIST=$SHELL_FOLDER/$DIRECT_IP_LIST
+RESERVED_IP_LIST=$SHELL_FOLDER/$RESERVED_IP_LIST
 DIRECT_DOMAINS=$SHELL_FOLDER/$DIRECT_DOMAINS
 FORCE_DIRECT_DOMAINS=$SHELL_FOLDER/$FORCE_DIRECT_DOMAINS
 ALLOW_PROXY_DEVICE=$SHELL_FOLDER/$ALLOW_PROXY_DEVICE
@@ -58,6 +61,8 @@ echo "ipset destroy $DIRECT_IPSET" >> ${DIRECT_IPSET_RULES}
 echo "ipset create $DIRECT_IPSET hash:net hashsize 16384 maxelem 262144" >> ${DIRECT_IPSET_RULES}
 # add direct ip to ipset
 cat ${DIRECT_IP_LIST} | awk -v DIRECT_IPSET=$DIRECT_IPSET '{print "ipset add " DIRECT_IPSET " " $0}' >> ${DIRECT_IPSET_RULES}
+# add reserved ip to ipset
+cat ${RESERVED_IP_LIST} | awk -v DIRECT_IPSET=$DIRECT_IPSET '{print "ipset add " DIRECT_IPSET " " $0}' >> ${DIRECT_IPSET_RULES}
 chmod +x ${DIRECT_IPSET_RULES}
 echo "generate ipset rules done"
 
@@ -103,16 +108,16 @@ echo "conf-file $DNS_DIRECT_DOMAIN_RULES" >> ${DNS_CONF}
 # generate iptables rules:
 # 1. forwarding traffic with overseas destination to proxy gateway
 # 2. forwarding dns request from specific mac address to proxy gateway
-IPTABLES_PROXY_RULE_TEMPLATE="iptables -A PREROUTING -t mangle \\
+IPTABLES_PROXY_RULE_TEMPLATE="-A PREROUTING -t mangle \\
     -m mac --mac-source MAC_ADDRESS \\
     -m set ! --match-set DIRECT_IPSET dst \\
     -j MARK --set-mark PROXY_MARK"
-DNS_UDP_PROXY_RULE_TEMPLATE="iptables -t nat -A PREROUTING \\
+DNS_UDP_PROXY_RULE_TEMPLATE="-t nat -A PREROUTING \\
     -m mac --mac-source MAC_ADDRESS \\
     -p udp --dport SRC_PORT \\
     -j REDIRECT \\
     --to-ports DST_PORT"
-DNS_TCP_PROXY_RULE_TEMPLATE="iptables -t nat -A PREROUTING \\
+DNS_TCP_PROXY_RULE_TEMPLATE="-t nat -A PREROUTING \\
     -m mac --mac-source MAC_ADDRESS \\
     -p tcp --dport SRC_PORT \\
     -j REDIRECT \\
@@ -121,21 +126,31 @@ echo "generate iptables rules"
 echo "#!/bin/sh" > ${IPTABLES_RULES}
 while read MAC_ADDRESS
 do
-    echo "$IPTABLES_PROXY_RULE_TEMPLATE" | \
+    echo "iptables $IPTABLES_PROXY_RULE_TEMPLATE" | \
         sed 's/MAC_ADDRESS/'$MAC_ADDRESS'/' | \
         sed 's/DIRECT_IPSET/'$DIRECT_IPSET'/' | \
         sed 's/PROXY_MARK/'$PROXY_MARK'/' >> ${IPTABLES_RULES}
 done < $ALLOW_PROXY_DEVICE
 while read MAC_ADDRESS
 do
-    echo "$DNS_UDP_PROXY_RULE_TEMPLATE" | \
+    echo "iptables $DNS_UDP_PROXY_RULE_TEMPLATE" | \
         sed 's/MAC_ADDRESS/'$MAC_ADDRESS'/' | \
         sed 's/SRC_PORT/'$DNS_DIRECT_PORT'/' | \
         sed 's/DST_PORT/'$DNS_PROXY_PROT'/' >> ${IPTABLES_RULES}
-    echo "$DNS_TCP_PROXY_RULE_TEMPLATE" | \
+    echo "iptables $DNS_TCP_PROXY_RULE_TEMPLATE" | \
         sed 's/MAC_ADDRESS/'$MAC_ADDRESS'/' | \
         sed 's/SRC_PORT/'$DNS_DIRECT_PORT'/' | \
         sed 's/DST_PORT/'$DNS_PROXY_PROT'/' >> ${IPTABLES_RULES}
+    if [ $ENABLE_IPV6_DNS_SERVER == 'true' ]; then
+        echo "ip6tables $DNS_UDP_PROXY_RULE_TEMPLATE" | \
+            sed 's/MAC_ADDRESS/'$MAC_ADDRESS'/' | \
+            sed 's/SRC_PORT/'$DNS_DIRECT_PORT'/' | \
+            sed 's/DST_PORT/'$DNS_PROXY_PROT'/' >> ${IPTABLES_RULES}
+        echo "ip6tables $DNS_TCP_PROXY_RULE_TEMPLATE" | \
+            sed 's/MAC_ADDRESS/'$MAC_ADDRESS'/' | \
+            sed 's/SRC_PORT/'$DNS_DIRECT_PORT'/' | \
+            sed 's/DST_PORT/'$DNS_PROXY_PROT'/' >> ${IPTABLES_RULES}
+    fi
 done < $ALLOW_PROXY_DEVICE
 chmod +x ${IPTABLES_RULES}
 echo "generate iptables rules done"
@@ -159,7 +174,8 @@ echo "sh $IP_ROUTE_RULES" >> ${STARTUP_SCRIPT}
 echo "#execute iptables rules" >> ${STARTUP_SCRIPT}
 echo "[ -z \"\$(cat $FIREWALL_USER_CONFIG | grep \"sh $IPTABLES_RULES\")\" ] \\
     && echo \"\" >> $FIREWALL_USER_CONFIG \\
-    && echo \"sh $IPTABLES_RULES\" >> $FIREWALL_USER_CONFIG" >> ${STARTUP_SCRIPT}
+    && echo \"sh $IPTABLES_RULES\" >> $FIREWALL_USER_CONFIG
+    && fw3 restart" >> ${STARTUP_SCRIPT}
 echo "fw3 restart" >> ${STARTUP_SCRIPT}
 echo "#smartdns conf" >> ${STARTUP_SCRIPT}
 # echo "cp $SHELL_FOLDER/smartdns.conf /var/etc/smartdns/" >> ${STARTUP_SCRIPT}
