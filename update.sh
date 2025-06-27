c94c16faa94ff5af449405156e29e12873a9b778
muffin <chuiqinfan@hotmail.com>
2025-06-28 01:13:21 +0800

    update

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
 echo "shell foler: $SHELL_FOLDER"
 chmod +x ${SHELL_FOLDER}/fetch.sh
 chmod +x ${SHELL_FOLDER}/generate.sh
+chmod +x ${SHELL_FOLDER}/init.sh
 
 ${SHELL_FOLDER}/fetch.sh
 ${SHELL_FOLDER}/generate.sh
+${SHELL_FOLDER}/init.sh
 
